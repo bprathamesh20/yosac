714e3de01af7b728c38e0a1b41234d8a18a40c5f
bprathamesh20 <bprathamesh2003@gmail.com>
2025-05-13 03:18:46 +0530

    fix css

--- components/message.tsx
+++ components/message.tsx
@@ -129,7 +129,7 @@ const PurePreviewMessage = ({
                       <div
                         data-testid="message-content"
                         className={cn('flex flex-col gap-4', {
-                          'bg-primary-message text-primary-foreground px-3 py-2 rounded-xl':
+                          'primary-message-bg text-primary-message-foreground px-3 py-2 rounded-xl':
                             message.role === 'user',
                         })}
                       >
